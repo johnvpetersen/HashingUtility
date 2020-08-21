7ec4f1ba64edee922e34ec2e163fb30a0e96df90
John V. Petersen <johnvpetersen@gmail.com>
2020-08-21 15:52:35 -0400

    Added new test

---
+++ App/root.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using static Newtonsoft.Json.JsonConvert;
+
+
+namespace App
+{
+    public abstract class root : object
+    {
+        private int _hash;
+
+
+        public root() {
+            _hash = ComputeHash(this);
+        }
+
+        public override string ToString()
+        {
+            return SerializeObject(this);
+        }
+
+        public override bool Equals(object obj) => ComputeHash(obj) == GetHashCode();
+
+        private int ComputeHash(object obj)
+        {
+            Int32 retVal = 0;
+
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(SerializeObject(obj)));
+                if (BitConverter.IsLittleEndian)
+                    Array.Reverse(bytes);
+
+                retVal =  BitConverter.ToInt32(bytes, 0);
+            };
+
+            return retVal;    
+
+        }
+
+       public int Hash => _hash;
+
+        public override int GetHashCode() => ComputeHash(this);
+
+
+    }
+
+}
+
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using App;
 using Xunit;
 
 namespace Tests
@@ -6,9 +7,29 @@ namespace Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ObjectsWithEquivPropsAreEqual()
         {
-
+            Assert.True(TestClass.Create(1,"One").Equals(TestClass.Create(1,"One")));
         }
+
+        [Fact]
+        public void HashPropertyIsPopulated()
+        {
+            Assert.True(TestClass.Create(1,"One").Hash == 491241268);
+        }
+
+
+    }
+
+    public class TestClass : root {
+        public TestClass(int amount, string description) : base() {
+            Amount = amount;
+            Description = description;
+        }
+
+        public int Amount {get; private set;}
+        public string Description {get; private set;}
+
+        public static TestClass Create(int amount, string description) => new TestClass(amount,description);
     }
 }
