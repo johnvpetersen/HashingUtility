420f93088e09e8be889e76f438c383d9e995b88f
John V. Petersen <johnvpetersen@gmail.com>
2020-08-22 17:33:05 -0400

    Revised tests, added new root constructor

--- App/root.cs
+++ App/root.cs
@@ -1,48 +1,69 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Text;
+using Newtonsoft.Json;
 using static Newtonsoft.Json.JsonConvert;
 
 
 namespace App
 {
-    public abstract class root : object
+    public class Root<T> : object
     {
-        private int _hash;
 
+        private readonly Int32 _hash;
 
-        public root() {
-            _hash = ComputeHash(this);
+        private readonly T _data;
+
+        [JsonConstructor]
+        public Root(T data, Int32 hash) {
+            _data = data;
+            _hash = hash;
         }
 
+        public Root(T data)
+        {
+            _data = data;
+            _hash = computeHash(_data);
+        }
         public override string ToString()
         {
-            return SerializeObject(this);
+            return SerializeObject(new { Data = _data, Hash = _hash });
         }
 
-        public override bool Equals(object obj) => ComputeHash(obj) == GetHashCode();
+        public override bool Equals(object obj) => obj.GetHashCode() == _hash;
 
-        private int ComputeHash(object obj)
+        private static int computeHash(object obj)
         {
-            Int32 retVal = 0;
 
+            Int32 retVal = 0;
             using (var sha256 = SHA256.Create())
             {
-                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(SerializeObject(obj)));
+                var bytes =
+                  sha256.ComputeHash(
+                      Encoding.UTF8.GetBytes(
+                          SerializeObject(obj)
+                          )
+                          );
                 if (BitConverter.IsLittleEndian)
                     Array.Reverse(bytes);
 
-                retVal =  BitConverter.ToInt32(bytes, 0);
+                retVal = BitConverter.ToInt32(bytes, 0);
             };
 
-            return retVal;    
+            return retVal;
 
         }
 
-       public int Hash => _hash;
+        public bool HasChanged() => computeHash(_data) != _hash;
 
-        public override int GetHashCode() => ComputeHash(this);
+        public override int GetHashCode() => computeHash(_data);
 
+        public static Root<T> Create(T data) {
+            return new Root<T>(data);
+        }
+
+        public T Data => _data;
+        public Int32 Hash => _hash;
 
     }
 
