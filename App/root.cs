497b01ec7d6dad3c0389582cfe3bf742dec18fb7
John V. Petersen <johnvpetersen@gmail.com>
2020-08-22 22:14:57 -0400

    Formatted code

--- App/root.cs
+++ App/root.cs
@@ -4,22 +4,18 @@ using System.Text;
 using Newtonsoft.Json;
 using static Newtonsoft.Json.JsonConvert;
 
-
 namespace App
 {
     public class Root<T> : object
     {
-
         private readonly Int32 _hash;
-
         private readonly T _data;
-
         [JsonConstructor]
-        public Root(T data, Int32 hash) {
+        public Root(T data, Int32 hash)
+        {
             _data = data;
             _hash = hash;
         }
-
         public Root(T data)
         {
             _data = data;
@@ -29,12 +25,9 @@ namespace App
         {
             return SerializeObject(new { Data = _data, Hash = _hash });
         }
-
         public override bool Equals(object obj) => obj.GetHashCode() == _hash;
-
         private static int computeHash(object obj)
         {
-
             Int32 retVal = 0;
             using (var sha256 = SHA256.Create())
             {
@@ -46,26 +39,21 @@ namespace App
                           );
                 if (BitConverter.IsLittleEndian)
                     Array.Reverse(bytes);
-
                 retVal = BitConverter.ToInt32(bytes, 0);
             };
-
             return retVal;
-
         }
-
         public bool HasChanged() => computeHash(_data) != _hash;
-
         public override int GetHashCode() => computeHash(_data);
-
-        public static Root<T> Create(T data) {
+        public static Root<T> Create(T data)
+        {
             return new Root<T>(data);
         }
-
+        public static Root<T> Create(T data, Int32 hash)
+        {
+            return new Root<T>(data, hash);
+        }
         public T Data => _data;
         public Int32 Hash => _hash;
-
     }
-
 }
-
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -9,53 +9,21 @@ namespace Tests
 {
     public class UnitTest1
     {
-
-        [Fact]
-        public void CanInstantiateDateTime() {
-
-            var sut = new Root<DateTime>(DateTime.Now);
-
-            var x = sut.Data;
-
-
-
-        }
-
-
-
         [Fact]
         public void CanDetectChangedData() {
-
             var sut = new Root<string>("test");
             var json = sut.ToString().Replace("test","TEST");
             sut = DeserializeObject<Root<string>>(json);
             Assert.True(sut.HasChanged());
-
         }
-
-
 
        [Fact]
        public void Test() {
            var sut = new TestClass2(1,"One");
-
            var json = SerializeObject(sut);
-
            sut = new TestClass2(JObject.Parse(json));
-
-        
            Assert.Equal(json, sut.ToString());
-
-
        } 
-
-
-
-
-
-
-
-
 
         [Fact]
         public void UnchangedObjectHasChangedIsFalse() {
@@ -63,57 +31,35 @@ namespace Tests
             Assert.False(sut.HasChanged());
         }
 
-
-
-
         [Fact]
         public void CanInstantiateNewInstance()
         {
             var actual = new Root<TestClass>(new TestClass(1,"one")).ToString();
             var expected = "{\"Data\":{\"Amount\":1,\"Description\":\"one\"},\"Hash\":308362742}";
-
             Assert.Equal(expected,actual);
         }
-
-
     }
 
-
-
     public class TestClass2    {
-
-
         public TestClass2(JObject jObject)  {
             Amount = jObject.SelectToken("Amount").ToObject<Root<int>>();    //DeserializeObject<T>(jObject.SelectToken("Data").ToString());
             Description = jObject.SelectToken("Description").ToObject<Root<string>>();
         }
-
-
-
         [JsonConstructor]
         public TestClass2(int amount, string description)  {
             Amount = Root<int>.Create(amount);
             Description = Root<string>.Create(description);
         }
-
         public override string ToString() => SerializeObject(this);
-
-
         public Root<int> Amount {get; private set;}
         public Root<string> Description {get; private set;}
-
     }   
 
-
-
     public class TestClass    {
-
-
         public TestClass(int amount, string description)  {
             Amount = amount;
             Description = description;
         }
-
         public int Amount {get; private set;}
         public string Description {get; private set;}
 
